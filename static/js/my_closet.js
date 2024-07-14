08336983e025059077168828809d135e25f42337
YashikaGupta09 <guptayashika2019@gmail.com>
2024-07-14 12:44:15 +0530

    my_closet improved

--- static/js/my_closet.js
+++ static/js/my_closet.js
@@ -1,10 +1,6 @@
-function updateCurrentTheme() {
-    const themeFilter = document.getElementById('theme-filter');
-    const currentTheme = document.getElementById('current-theme');
-    currentTheme.textContent = `Current Theme: ${themeFilter.value}`;
-}
-
 function removeDesign(designId) {
     const designBox = document.getElementById(designId);
-    designBox.remove();
+    if (designBox) {
+        designBox.remove();
+    }
 }
