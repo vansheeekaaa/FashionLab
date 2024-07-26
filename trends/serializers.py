39939fa8329d0583ce12b300d4a0c696124115dd
vansheeekaaa <vanshiika04@gmail.com>
2024-07-26 15:08:56 +0530

    serialisers.py

--- trends/serializers.py
+++ trends/serializers.py
@@ -1,22 +1,7 @@
 from rest_framework import serializers
-from .models import Design, User, Vote, Interaction
+from .models import DesignSubmission
 
-class DesignSerializer(serializers.ModelSerializer):
+class DesignSubmissionSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Design
-        fields = '__all__'
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = '__all__'
-
-class VoteSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Vote
-        fields = '__all__'
-
-class InteractionSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Interaction
+        model = DesignSubmission
         fields = '__all__'
