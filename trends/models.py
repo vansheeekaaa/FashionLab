7a37bfd9773d66511c523f4ed607be0a789e5040
vansheeekaaa <vanshiika04@gmail.com>
2024-07-28 01:36:20 +0530

    Auto-fill during design submission

--- trends/models.py
+++ trends/models.py
@@ -4,7 +4,6 @@ from django.db import models
 class DesignSubmission(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField()
-    password = models.CharField(max_length=100)
     design_link = models.URLField()
     submitted_at = models.DateTimeField(auto_now_add=True)
     votes = models.IntegerField(default=0)
